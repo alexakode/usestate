5be20bb809e34aa3233b682eb24d7b6bffe6bb04
Alexander <alexanderm.kodehode@gmail.com>
2025-09-15 13:32:00 +0200

    eo lesson

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,27 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 function App() {
   const [text, setText] = useState("");
-  const updateText = (e) => {
-    setText(e.target.value);
-    console.log(text);
+  const [list, setList] = useState([]);
+  // checks if there is anything in text and replaces list with a new array containing the old list and the new text
+  const updateList = () => {
+    if (text.trim() !== "") {
+      setList((prevList) => [...prevList, text]);
+      setText("");
+    }
   };
   return (
     <>
-      <input type="text" onChange={(e) => setText(e.target.value)} />
-      <button>Add</button>
-      <ul>Display</ul>
+      <input
+        value={text}
+        type="text"
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button onClick={updateList}>Add</button>
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </>
   );
 }
