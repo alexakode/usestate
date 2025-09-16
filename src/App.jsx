1a8336bd21953ea1aa3f15972e6e16a0461e434e
alexakode <alexanderm.kodehode@gmail.com>
2025-09-16 13:10:42 +0200

    react visibility toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,17 @@
+// hooks, state
+// vi kan importere hooks fra react; useXXX
+// state is immutable, conditional rendering
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-function App() {
-  const [text, setText] = useState("");
-  const [list, setList] = useState([]);
-  // checks if there is anything in text and replaces list with a new array containing the old list and the new text
-  const updateList = () => {
-    if (text.trim() !== "") {
-      setList((prevList) => [...prevList, text]);
-      setText("");
-    }
-  };
+
+export default function App() {
+  const [isVisible, setIsVisible] = useState(true);
+  const toggleVisibility = () => setIsVisible();
   return (
-    <>
-      <input
-        value={text}
-        type="text"
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button onClick={updateList}>Add</button>
-      <ul>
-        {list.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </>
+    <div>
+      <button onClick={toggleVisibility}>
+        {isVisible ? "Hide" : "Show"} Message
+      </button>
+      {isVisible && <p>This is a toggled message!</p>}
+    </div>
   );
 }
-
-export default App;
