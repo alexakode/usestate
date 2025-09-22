d97332f1786e55f3171071a84522c4f08e00b305
alexakode <alexanderm.kodehode@gmail.com>
2025-09-22 14:18:40 +0200

    justert useLocalStorage, funker ikke

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,8 +1,13 @@
 import { useState, useEffect } from "react";
 export default function useLocalStorage(key, initialValue) {
-  const [value, setValue] = useState((key, initialValue) => {
-    const stored = localStorage.getItem(key);
-    return stored ? JSON.parse(stored) : initialValue;
+  const [value, setValue] = useState(() => {
+    try {
+      const stored = localStorage.getItem(key);
+      return stored ? JSON.parse(stored) : initialValue;
+    } catch (error) {
+      console.error("Error reading localStorage:", error);
+      return initialValue;
+    }
   });
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
